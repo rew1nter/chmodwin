14318af7223ebfc23506e51a450b7add7e704929
Rewinter <64508791+rew1nter@users.noreply.github.com>
2022-02-08 13:06:23 +0600

    Create chmodwin.go

---
+++ chmodwin.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type chmod struct {
+	path string
+	args []string
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println("This can 'chmod 600' files in Windows\nusage:\n\tchmod key.pem\n\tchmod.exe key.pem")
+	}
+	flag.Parse()
+	if flag.NArg() > 1 {
+		fmt.Println("ERR: You have to provide only one argument")
+		os.Exit(1)
+	} else if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	filePath := flag.Args()[0]
+	//fmt.Println(filePath)
+	c1 := chmod{
+		path: "icacls.exe",
+		args: []string{filePath, "/reset"},
+	}
+
+	u := username()
+	c2 := chmod{
+		path: "icacls.exe",
+		args: []string{filePath, "/GRANT:R", fmt.Sprintf("%v:(R)", u)},
+	}
+
+	c3 := chmod{
+		path: "icacls.exe",
+		args: []string{filePath, "/inheritance:r"},
+	}
+	execute(c1.path, c1.args)
+	execute(c2.path, c2.args)
+	execute(c3.path, c3.args)
+
+}
+
+func username() (x string) {
+	for _, j := range os.Environ() {
+		s := strings.Split(j, "=")
+		if s[0] == "USERNAME" {
+			x = s[1]
+		}
+	}
+	return x
+}
+
+func execute(name string, args []string) {
+	cmd := exec.Command(name, args...)
+
+	cmd.Stdin = os.Stdin
+	//cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
